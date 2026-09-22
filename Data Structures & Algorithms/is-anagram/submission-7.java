58047ade06c87b7d212d876648ef73a23903556d
Raunak Priya <raunakpriya@gmail.com>
2026-09-22 16:10:23 +0530

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> hash = new HashMap<>();
+
+        for(int i = 0 ; i < s.length() ; i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+            hash.put(t.charAt(i), hash.getOrDefault(t.charAt(i), 0) + 1);
+        }
+        return map.equals(hash);
+
+    }
+}
